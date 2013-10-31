7b7da8d03c867de73f9919a85ce66d8b68dff4e1
PeterChoi <ptc113@gmail.com>
2013-11-01 04:13:14 +0900

    1. Update really good login script.  by. ggg

--- js/peter.js
+++ js/peter.js
@@ -1,16 +1,30 @@
 (function($) {
-	$.post(
-	{
-		'school_id' : $('#inputEmail').val()
-	},
-	function(response) {
-		if (response.status === 0) {
-			// Success!
+	$(function() {
+		$('.form-horizontal').on('submit', function() {
+			if ($('#inputStudentNum').val() === "") {
+				alert('Please enter username!!');
+				return false;
+			}
 			
-		}
-		else {
-			// Fail.
-			alert(response.status);
-		}
-	}, 'json');
+			$.post('/user/login',
+			{
+				'school_id' : $('#inputStudentNum').val()
+			},
+			function(response) {
+				if (response.status === 0) {
+					// Success!
+					//console.log(response.data);
+					//alert(JSON.stringify(response.data));
+					location.href = "index.html";
+					//var term = $('#inputStudentNum').val();
+				}
+				else {
+					// Fail.
+					alert(response.data);
+				}
+			}, 'json');
+			
+			return false;
+		});
+	});
 })(jQuery);
