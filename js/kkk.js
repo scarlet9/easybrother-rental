6ecf214f4e2ff254a119031299aaa7ebd7a409c4
KyeongKoo Kang <cockroach419@gmail.com>
2013-11-07 08:06:15 +0900

    마지막
    마지막

--- js/kkk.js
+++ js/kkk.js
@@ -275,6 +275,8 @@ function geo_success_logs(latit, longi){
 			
 			//location.href = '/reservation';
 			alert('반납에 성공하였습니다. 이용해 주셔서 감사합니다.');
+			$('.log-state-2').find('td').last().text(state_string[3]);
+			$('#modal-return-zone').remove();
 			
 		}, 'json')
 		.fail(function(jqxhr) {
