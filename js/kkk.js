5c0e66e3234efceefdebbf6618b7fae197a3bbb6
KyeongKoo Kang <cockroach419@gmail.com>
2013-11-07 08:11:08 +0900

    ㄱㄱㄱㄱㄱㄱㄱㄱ
    ㄱㄱㄱㄱ

--- js/kkk.js
+++ js/kkk.js
@@ -242,7 +242,7 @@ function geo_success_now(latit, longi) {
 
 }
 
-function geo_success_logs(latit, longi){
+function geo_success_logs(latit, longi, state_string){
 	var nTemp = [];
 	var rTemp = [];
 	jQuery.get('/racks', function(response) {
